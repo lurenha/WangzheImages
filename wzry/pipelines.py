b69bad73c3f4d2f4f663ff24ba9e352b12281233
weipeng01 <weipeng@neurondms.com>
2020-10-20 17:05:57 +0800

    拉去英雄所有image

--- wzry/pipelines.py
+++ wzry/pipelines.py
@@ -6,21 +6,22 @@
 
 # useful for handling different item types with a single interface
 import os
+import re
 import scrapy
 from scrapy.pipelines.images import ImagesPipeline
 from wzry.settings import IMAGES_STORE as images_store
 
 
-
 class WzryPipeline(ImagesPipeline):
     # 配置下载图片的url
     def get_media_requests(self, item, info):
-        print(item)
         image_url = item['image_url']
-        yield scrapy.Request(image_url)
+        return [scrapy.Request(re.sub("\d(?=.jpg)", str(i), image_url)) for i in range(1, 10)]
 
     # 设置下载到本地的文件name
     def item_completed(self, results, item, info):
         image_path = [x["path"] for ok, x in results if ok]
-        os.rename(images_store+image_path[0],images_store+item["name"]+".jpg")
+        print(image_path)
+        for index in range(len(image_path)):
+            os.rename(images_store + image_path[index], images_store + item["name"]+"-"+ str(index+1)+ ".jpg")
         return item
